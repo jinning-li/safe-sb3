9063f496f02e7db5213fdb2fd44efb051534fe0d
jinning-li <jinning_li@berkeley.edu>
2023-02-21 21:24:47 -0800

    Add BC

---
+++ stable_baselines3/bc/__init__.py
@@ -0,0 +1,4 @@
+from stable_baselines3.ppo.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from stable_baselines3.bc.bc import BC
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "PPO"]
